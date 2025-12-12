0f09023c1a7746f32b71932552a72149f82142bc
dongyun <863234514@qq.com>
2025-12-12 15:29:58 +0800

    minor

--- weight_conversion.py
+++ weight_conversion.py
@@ -9,7 +9,7 @@ for key, value in state_dict.items():
     elif key.startswith('visual.'):
         new_key = key.replace('visual.', 'model.visual.')
     else:
-        raise ValueError(f'Unexpected key {key}')
+        new_key = key
     new_state_dict[new_key] = value
 
 torch.save(new_state_dict, 'assets/reward_model.pt')
